77625622b3bf98c270c1a68fbfc68a2463cc10fe
Matheus Barbosa <mxtheusbm@gmail.com>
2023-02-03 19:55:06 -0300

    feat(login): validate all fields

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,21 +1,36 @@
-export type InputProps = {
-  label: string;
-  placeholder: string;
-  type: string;
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+export interface IFormValues {
+  email: string;
+  password: string;
 }
 
-function Input({label, placeholder, type}: InputProps) {
+type InputProps = {
+  label?: string;
+  name: "email" | "password"
+  placeholder?: string;
+  type: string;
+  register: UseFormRegister<IFormValues>;
+  errors?: string | undefined;
+};
+
+function Input({ label, name, placeholder, type, register, errors }: InputProps) {
   return (
     <div className="flex flex-col gap-2">
       <label className="text-xs font-medium text-gray-800">
         {label}
       </label>
       
-      <input
-        className="bg-green-50 py-3 px-5 rounded-md border-none" 
-        type={type}
-        placeholder={placeholder}
-      />
+      <div className="flex flex-col gap-1">
+        <input
+          className={`bg-green-50 py-3 px-5 rounded-md outline-none ${errors && "border-[1px] border-red-500"}`}
+          type={type}
+          placeholder={placeholder}
+          {...register(name)}
+        />
+
+        {errors && <span className="text-xs text-red-500">{errors}</span>}
+      </div>
     </div>
   )
 }
