4c0b1531c39e03b38e484749af0a314ac289ed90
Casmei <casmei@protonmail.com>
2023-02-28 22:10:20 -0300

    feat(institution): add create institution

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { FC, ReactElement } from "react";
 import { Route, Routes } from "react-router-dom";
+import { CreateInstitution } from "./pages/instituion/create-institution";
 import Login from "./pages/login";
 import Register from "./pages/register";
-
+import { RoutesEnum } from "./utils/routesEnum"
 const Router: FC = (): ReactElement => {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path={RoutesEnum.LOGIN} element={<Login />} />
+      <Route path={RoutesEnum.REGISTER} element={<Register />} />
+      <Route path={RoutesEnum.CREATE_INSTITUTION} element={<CreateInstitution />} />
     </Routes>
   )
 }
